3c9e7bfbcbf69e2532aaac56a3349d6612b4df0c
Sinevia <info@sinevia.com>
2019-05-31 07:33:51 +0100

    Update LinkUtils.php

--- src/LinkUtils.php
+++ src/LinkUtils.php
@@ -1,1 +1,21 @@
 <?php
+
+namespace Sinevia;
+
+class LinkUtils {
+    protected static function baseUrl() {
+        return \Sinevia\Registry::get("URL_BASE", 'localhost');
+    }
+
+    private static function buildUrl($path, $queryData = []) {
+        return self::baseUrl() . '/' . trim($path, '/') . self::query($queryData);
+    }
+  
+    private static function query($queryData = []) {
+        $queryString = '';
+        if (count($queryData)) {
+            $queryString = '?' . http_build_query($queryData);
+        }
+        return $queryString;
+    }
+}
