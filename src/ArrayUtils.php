946263e19b6387a96f67909a209173a0ab40de69
Sinevia <info@sinevia.com>
2019-08-08 04:07:36 +0100

    Update ArrayUtils.php

--- src/ArrayUtils.php
+++ src/ArrayUtils.php
@@ -1,9 +1,17 @@
-function array_columns(array $arr, array $keysSelect)
-{    
-    $keys = array_flip($keysSelect);
-    $filteredArray = array_map(function($a) use($keys){
-        return array_intersect_key($a,$keys);
-    }, $arr);
+<?php
+namespace Sinevia;
 
-    return $filteredArray;
+class ArrayUtils {
+    /**
+     * Returns the speified columns
+     */
+    function columns(array $arr, array $keysSelect)
+    {    
+        $keys = array_flip($keysSelect);
+        $filteredArray = array_map(function($a) use($keys){
+            return array_intersect_key($a,$keys);
+        }, $arr);
+
+        return $filteredArray;
+    }
 }
