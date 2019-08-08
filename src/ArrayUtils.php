063cfc4ff18241162387be3a08961c109234cede
Sinevia <info@sinevia.com>
2019-08-08 04:05:19 +0100

    Create ArrayUtils.php

---
+++ src/ArrayUtils.php
@@ -0,0 +1,9 @@
+function array_columns(array $arr, array $keysSelect)
+{    
+    $keys = array_flip($keysSelect);
+    $filteredArray = array_map(function($a) use($keys){
+        return array_intersect_key($a,$keys);
+    }, $arr);
+
+    return $filteredArray;
+}
