a11051b9c38a8f7f07ca4238b8213d7600819d52
Milan Lesichkov <lesichkovm@gmail.com>
2020-03-09 07:08:27 +0000

    Added tests for LinkUtils

--- src/LinkUtils.php
+++ src/LinkUtils.php
@@ -3,19 +3,54 @@
 namespace Sinevia;
 
 class LinkUtils {
+    protected static $baseUrl = "";
     
-    protected static function baseUrl() {
-        return \Sinevia\Registry::get("URL_BASE", 'localhost');
+    public static function getBaseUrl() {
+        return self::$baseUrl;
+    }
+
+    public static function setBaseUrl($baseUrl) {
+        return self::$baseUrl = $baseUrl;
     }
 
     public static function buildUrl($path, $queryData = []) {
-        $url = self::baseUrl() . '/' . trim($path, '/') . self::query($queryData);
-
-        if (StringUtils::startsWith($url, 'http://') == false and StringUtils::startsWith($url, 'https://') == false) {
-            $serverProtocol = $_SERVER['SERVER_PROTOCOL'] ?? '';
-            $protocol = stripos($serverProtocol, 'https') === 0 ? 'https://' : 'http://';
-            $url = $protocol . $url;
+        $isUrl = (StringUtils::startsWith($path, 'http://') or StringUtils::startsWith($path, 'https://'));
+        
+        if ($isUrl == false) {
+            $url = self::getBaseUrl() . '/' . trim($path, '/');
+            $url = ($url!='/') ? \rtrim($url,'/') : '/';
+            if(self::getBaseUrl() != ''){
+                if (StringUtils::startsWith(self::getBaseUrl(), 'http://') == false and StringUtils::startsWith(self::getBaseUrl(), 'https://') == false) {
+                    $serverProtocol = $_SERVER['SERVER_PROTOCOL'] ?? '';
+                    $protocol = stripos($serverProtocol, 'https') === 0 ? 'https://' : 'http://';
+                    $url = $protocol . $url;
+                }
+            }
+        } else {
+            $url = $path;
         }
+
+        $queryString = http_build_query($queryData);
+
+        if ($queryString == "") {
+            return $url;
+        }
+
+        $separator = (strpos($url, '?') === false) ? '?' : '&';
+
+        $url = $url . $separator . $queryString;
+
+        return $url;
+
+        //     $protocol = stripos($serverProtocol, 'https') === 0 ? 'https://' : 'http://';
+
+        // $url = self::getBaseUrl() . '/' . trim($path, '/') . self::query($queryData);
+
+        // if (StringUtils::startsWith($url, 'http://') == false and StringUtils::startsWith($url, 'https://') == false) {
+        //     $serverProtocol = $_SERVER['SERVER_PROTOCOL'] ?? '';
+        //     $protocol = stripos($serverProtocol, 'https') === 0 ? 'https://' : 'http://';
+        //     $url = $protocol . $url;
+        // }
 
         return $url;
     }
