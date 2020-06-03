76fb189a33e65b590a9a5a25bfc53b659273ba26
Milan Lesichkov <lesichkovm@gmail.com>
2020-06-03 07:39:10 +0100

    Update Utils.php

--- src/Utils.php
+++ src/Utils.php
@@ -601,6 +601,58 @@ class Utils {
         }
         return $url;
     }
+    
+    /**
+     * Parses command line arguments
+     *
+     * <code>
+     *     $args = \Sinevia\Utils::parseArguments()['arguments'];
+     *     $parameters = \Sinevia\Utils::parseArguments()['parameters'];
+     * </code>
+     *
+     * @return array
+     */
+    public static function parseArguments($argv = null) {
+        $argv = $argv ? $argv : $_SERVER['argv'];
+        $o = ['arguments' => [], 'parameters' => []];
+
+        array_shift($argv);
+        $o = ['arguments' => [], 'parameters' => []];
+        for ($i = 0, $j = count($argv); $i < $j; $i++) {
+            $a = $argv[$i];
+            if (substr($a, 0, 2) == '--') {
+                $eq = strpos($a, '=');
+                if ($eq !== false) {
+                    $o['parameters'][substr($a, 2, $eq - 2)] = substr($a, $eq + 1);
+                } else {
+                    $k = substr($a, 2);
+                    if ($i + 1 < $j && $argv[$i + 1][0] !== '-') {
+                        $o['parameters'][$k] = $argv[$i + 1];
+                        $i++;
+                    } else if (!isset($o[$k])) {
+                        $o['parameters'][$k] = true;
+                    }
+                }
+            } else if (substr($a, 0, 1) == '-') {
+                if (substr($a, 2, 1) == '=') {
+                    $o['parameters'][substr($a, 1, 1)] = substr($a, 3);
+                } else {
+                    foreach (str_split(substr($a, 1)) as $k) {
+                        if (!isset($o[$k])) {
+                            $o['parameters'][$k] = true;
+                        }
+                    }
+                    if ($i + 1 < $j && $argv[$i + 1][0] !== '-') {
+                        $o['parameters'][$k] = $argv[$i + 1];
+                        $i++;
+                    }
+                }
+            } else {
+                $o['arguments'][] = $a;
+            }
+        }
+        return $o;
+    }
 
     /**
      * Calculates percentage given current position and total.
